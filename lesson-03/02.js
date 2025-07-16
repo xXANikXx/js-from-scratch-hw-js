552c345a2801e355f221e73d13eb9ba56fe2d017
ANik <annikita.and@gmail.com>
2025-07-16 19:31:21 +0500

    lesson3.HW

--- lesson-03/02.js
+++ lesson-03/02.js
@@ -2,9 +2,6 @@
 // Функция должна возвращать true, если число чётное, и false — в противном случае
 
 function isEven(a) {
-    if (a % 2 === 0) {
-        return true;
-    } else
-        return false;
+    return a % 2 === 0;
 }
 console.log(isEven());
